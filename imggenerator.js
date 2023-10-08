fffdc64095b80a2e0b65e6882998a785f11f44a3
rushik0507 <rushikpateljnv@gmail.com>
2023-10-08 14:57:17 +0530

    api learnings

---
+++ imggenerator.js
@@ -0,0 +1,47 @@
+// new script file
+
+const key = "MkPXlv0g3JhPcFqDmzvhYyZUnD-WEwQSoSfd4lXjiOg"
+
+const formel = document.getElementById("nav-form");
+const inputel = document.getElementById("search");
+const searchresult = document.querySelector('.team');
+const showmore = document.querySelector('.show-more');
+
+let inputdata = "";
+let page = 1;
+
+async function searchImage() {
+  inputdata = inputel.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}query=${inputdata}&client_id=${key}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  if (page === 1) {
+    searchresult.innerHTML = "";
+  }
+
+  results.map((result) => {
+    const image = document.createElement('img');
+    image.src = result.urls.small;
+    searchresult.appendChild(image);
+  });
+
+  page++;
+  if (page > 1) {
+    showmore.style.display = "block";
+  }
+}
+
+
+formel.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImage();
+});
+
+showmore.addEventListener("click", () => {
+  searchImage();
+});
